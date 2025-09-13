544a52f3c8597011fed345e6a62cc8d10dd06130
AxelPriet <axeprieto@uniboyaca.edu.co>
2025-09-13 11:16:24 -0500

    ciclos php

---
+++ if-switch.php
@@ -0,0 +1,25 @@
+<?php
+$edad = 21;
+
+
+
+
+
+elseif ($edad < 25) {
+    echo "mayor de edad";
+}
+
+$categoria = "a";
+switch ($categoria) {
+    case "a":
+        //codigo...
+        break;
+    case "b":
+        //codigo
+        break;
+    default:
+        //codigo
+        break;
+    }
+    ?>                
+
